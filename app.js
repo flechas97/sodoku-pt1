49f8e01173e478ee223ea0a3503e414c24dd2344
flechas97 <flechasarcon@gmail.com>
2022-01-27 19:15:15 +0100

    eventos

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 
 
 var tablero = [
-    [1, 2, 3, 2, 0, 0, 3, 0, 0],
-    [6, 5, 4, 0, 0, 0, 0, 0, 0],
-    [7, 8, 5, 0, 0, 0, 0, 0, 0],
-    [4, 0, 0, 5, 0, 0, 6, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [7, 0, 0, 8, 0, 0, 9, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [5, 3, "", "", 7, "", "", "", ""],
+    [6, "", "", 1, 9, 5, "", "", ""],
+    ["", 9, 8, "", "", "", "", 6, ""],
+    [8, "", "", "", 6, "", "", "", 3],
+    [4, "", "", 8, "", 3, "", "", 1],
+    [7, "", "", "", 2, "", "", "", 6],
+    ["", 6, "", "", "", "", 2, 8, ""],
+    ["", "", "", 4, 1, 9, "", "", 5],
+    ["", "", "", "", 8, "", "", 7, 9],
 ];
+console.log(tablero)
 //Funcion para llenar arrays con los datos de cada caja 3x3
-var caja1 = llenarcaja(0, 0);
-var caja2 = llenarcaja(0, 1);
-var caja3 = llenarcaja(0, 2);
-var caja4 = llenarcaja(1, 0);
-var caja5 = llenarcaja(1, 1);
-var caja6 = llenarcaja(1, 2);
-var caja7 = llenarcaja(2, 0);
-var caja8 = llenarcaja(2, 1);
-var caja9 = llenarcaja(2, 2);
+var caja0 = llenarcaja(0, 0);
+var caja1 = llenarcaja(0, 1);
+var caja2 = llenarcaja(0, 2);
+var caja3 = llenarcaja(1, 0);
+var caja4 = llenarcaja(1, 1);
+var caja5 = llenarcaja(1, 2);
+var caja6 = llenarcaja(2, 0);
+var caja7 = llenarcaja(2, 1);
+var caja8 = llenarcaja(2, 2);
 var fila0 = document.getElementsByClassName("r0");
 var fila1 = document.getElementsByClassName("r1");
 var fila2 = document.getElementsByClassName("r2");
@@ -43,31 +44,138 @@ cargarfilas(fila8, 8);
 
 for (let i = 0; i < fila0.length; i++) {
     fila0[i].addEventListener("focusout", () =>{
-        console.log("asd");
+        if(fila0[i].value != 0){
+            tablero[0][i] = parseInt(fila0[i].value);
+            if(getrow(tablero,0,parseInt(fila0[i].value)) == false){
+                fila0[i].value = 0;
+                tablero[0][i] = 0;
+            }else if(getcol(tablero,0,parseInt(fila0[i].value))==false){
+                fila0[i].value = 0;
+                tablero[0][i] = 0;
+            }
+            
+            validartodo();
+            
+            // if(getcol(tablero,0,parseInt(fila0[i].value))==false){
+            //     fila0[i].value = 0;
+            //     tablero[0][i] = 0;
+            // }
+            //getrow(tablero,0,parseInt(fila0[i].value));
+            // console.log("zona 0-"+ i);
+            // console.log(tablero);
+        }
     });
     fila1[i].addEventListener("focusout", () =>{
-        console.log("asd");
+        if(fila1[i].value != 0){
+            tablero[1][i] = parseInt(fila1[i].value);
+            if(getrow(tablero,1,parseInt(fila1[i].value)) == false){
+                fila1[i].value = 0;
+                tablero[1][i] = 0;
+            }else if(getcol(tablero,1,parseInt(fila1[i].value))==false){
+                fila1[i].value = 0;
+                tablero[1][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
     });
     fila2[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila2[i].value != 0){
+            tablero[2][i] = parseInt(fila2[i].value);
+            if(getrow(tablero,2,parseInt(fila2[i].value)) == false){
+                fila2[i].value = 0;
+                tablero[2][i] = 0;
+            }else if(getcol(tablero,2,parseInt(fila2[i].value))==false){
+                fila2[i].value = 0;
+                tablero[2][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 2-"+ i);
     });
     fila3[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila3[i].value != 0){
+            tablero[3][i] = parseInt(fila3[i].value);
+            if(getrow(tablero,3,parseInt(fila3[i].value)) == false){
+                fila3[i].value = 0;
+                tablero[3][i] = 0;
+            }else if(getcol(tablero,3,parseInt(fila3[i].value))==false){
+                fila3[i].value = 0;
+                tablero[3][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 3-"+ i);
     });
     fila4[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila4[i].value != 0){
+            tablero[4][i] = parseInt(fila4[i].value);
+            if(getrow(tablero,4,parseInt(fila4[i].value)) == false){
+                fila4[i].value = 0;
+                tablero[4][i] = 0;
+            }else if(getcol(tablero,4,parseInt(fila4[i].value))==false){
+                fila4[i].value = 0;
+                tablero[4][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 4-"+ i);
     });
     fila5[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila5[i].value != 0){
+            tablero[5][i] = parseInt(fila5[i].value);
+            if(getrow(tablero,5,parseInt(fila5[i].value)) == false){
+                fila5[i].value = 0;
+                tablero[5][i] = 0;
+            }else if(getcol(tablero,5,parseInt(fila5[i].value))==false){
+                fila5[i].value = 0;
+                tablero[5][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+            
+        }
+        // console.log("zona 5-"+ i);
     });
     fila6[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila6[i].value != 0){
+            tablero[6][i] = parseInt(fila6[i].value);
+            if(getrow(tablero,6,parseInt(fila6[i].value)) == false){
+                fila6[i].value = 0;
+                tablero[6][i] = 0;
+            }else if(getcol(tablero,6,parseInt(fila6[i].value))==false){
+                fila6[i].value = 0;
+                tablero[6][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 6-"+ i);
     });
     fila7[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila7[i].value != 0){
+            tablero[7][i] = parseInt(fila7[i].value);
+            if(getrow(tablero,7,parseInt(fila7[i].value)) == false){
+                fila7[i].value = 0;
+                tablero[7][i] = 0;
+            }else if(getcol(tablero,7,parseInt(fila7[i].value))==false){
+                fila7[i].value = 0;
+                tablero[7][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 7-"+ i);
     });
     fila8[i].addEventListener("focusout", () =>{
-        console.log("asd");
+           if(fila8[i].value != 0){
+            tablero[8][i] = parseInt(fila8[i].value);
+            if(getrow(tablero,8,parseInt(fila8[i].value)) == false){
+                fila8[i].value = 0;
+                tablero[8][i] = 0;
+            }else if(getcol(tablero,8,parseInt(fila8[i].value))==false){
+                fila8[i].value = 0;
+                tablero[8][i] = 0;
+            }
+            // console.log("zona 1-"+ i);
+        }
+        // console.log("zona 8-"+ i);
     });
 }
 function llenarcaja(row, col, caja = []) {
@@ -103,11 +211,13 @@ function getcaja(array, caja, numero) {
     }
     //console.log(indices);
     if (indices.length > 1) {
-        // alert(element+ " no es valido en la columna");
-        console.log(element + " no es valido en la caja")
+         alert(element+ " no es valido en la caja");
+        // console.log(element + " no es valido en la caja")
+        return false;
     } else {
         // alert(element+ " es valido en la columna");
         console.log(element + " es valido en la caja")
+        return true;
     }
 }
 
@@ -128,11 +238,13 @@ function getcol(array, col,numero) {
     }
     // console.log(indices);
     if (indices.length > 1) {
-        // alert(element+ " no es valido en la columna");
+         alert(element+ " no es valido en la columna");
         console.log(element + " no es valido en la columna")
+        return false;
     } else {
         // alert(element+ " es valido en la columna");
         console.log(element + " es valido en la columna")
+        return true;
     }
 }
 function getrow(array,row,numero) {
@@ -151,11 +263,13 @@ function getrow(array,row,numero) {
     }
     // console.log(indices);
     if (indices.length > 1) {
-        // alert(element+ " no es valido en la fila");
+         alert(element+ " no es valido en la fila");
         console.log(element + " no es valido en la fila")
+        return false;
     } else {
         // alert( element+ " es valido en la fila");
         console.log(element + " es valido en la fila")
+        return true;
     }
 }
 // getcol(tablero,0); //Funcion para validar una columna
@@ -172,31 +286,31 @@ function validcaja(numerocaja) {
     console.log("caja numero= "+numerocaja)
     switch (numerocaja) {
         case 0:
-            validarcaja(tablero, caja1);
+            validarcaja(tablero, caja0);
             break;
         case 1:
-            validarcaja(tablero, caja2);
+            validarcaja(tablero, caja1);
             break;
         case 2:
-            validarcaja(tablero, caja3);    
+            validarcaja(tablero, caja2);    
             break;
         case 3:
-            validarcaja(tablero, caja4);    
+            validarcaja(tablero, caja3);    
             break;
         case 4:
-            validarcaja(tablero, caja5);    
+            validarcaja(tablero, caja4);    
             break;
         case 5:
-            validarcaja(tablero, caja6);    
+            validarcaja(tablero, caja5);    
             break;
         case 6:
-            validarcaja(tablero, caja7);    
+            validarcaja(tablero, caja6);    
             break;
         case 7:
-            validarcaja(tablero, caja8);    
+            validarcaja(tablero, caja7);    
             break;
         case 8:
-            validarcaja(tablero, caja9);    
+            validarcaja(tablero, caja8);    
             break;
         default:
             break;
@@ -215,6 +329,6 @@ function validartodo() {
     }
 }
 
-validartodo();
+//validartodo();
 
 
